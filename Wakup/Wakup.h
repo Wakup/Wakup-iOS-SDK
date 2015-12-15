75e2187b90e8323e0880821852461385f6ccf077
Guillermo Gutiérrez Doral <guillermo.gutierrez@twincoders.com>
2015-12-15 18:05:27 +0100

    Initial Commit

---
+++ Wakup/Wakup.h
@@ -0,0 +1,19 @@
+//
+//  Wakup.h
+//  Wakup
+//
+//  Created by Guillermo Gutiérrez on 15/12/15.
+//  Copyright © 2015 Yellow Pineapple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Wakup.
+FOUNDATION_EXPORT double WakupVersionNumber;
+
+//! Project version string for Wakup.
+FOUNDATION_EXPORT const unsigned char WakupVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Wakup/PublicHeader.h>
+
+
