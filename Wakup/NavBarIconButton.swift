dfef515d994113445238029dfb87235345de93d8
Guillermo Gutiérrez <guillermo.gutierrez@twincoders.com>
2016-01-07 12:56:24 +0100

    Navigation bar icons are now customizable using UIAppearance proxies

---
+++ Wakup/NavBarIconButton.swift
@@ -0,0 +1,13 @@
+//
+//  NavBarIconButton.swift
+//  Wakup
+//
+//  Created by Guillermo Gutiérrez on 5/1/16.
+//  Copyright © 2016 Yellow Pineapple. All rights reserved.
+//
+
+import Foundation
+
+public class NavBarIconButton: CodeIconButton {
+    
+}
