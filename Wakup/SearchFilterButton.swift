3b55937c37ada41e5afec40ebea1f4141c5fb825
Guillermo Gutiérrez <guillermo.gutierrez@twincoders.com>
2016-01-07 17:21:27 +0100

    Created SearchFilterButton class to allow easy UIAppearance proxy customization of search filter buttons

---
+++ Wakup/SearchFilterButton.swift
@@ -0,0 +1,14 @@
+//
+//  SearchFilterButton.swift
+//  Wakup
+//
+//  Created by Guillermo Gutiérrez on 7/1/16.
+//  Copyright © 2016 Yellow Pineapple. All rights reserved.
+//
+
+import Foundation
+
+@IBDesignable
+public class SearchFilterButton: CenteredCodeIconButton {
+    
+}
