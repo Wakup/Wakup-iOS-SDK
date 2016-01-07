093c6b693502144c62c3be0a2322f81803d747e6
Guillermo Gutiérrez <guillermo.gutierrez@twincoders.com>
2016-01-07 12:58:19 +0100

    Created CouponActionButton class to allow easy UIAppearance proxy customization

---
+++ Wakup/CouponActionButton.swift
@@ -0,0 +1,13 @@
+//
+//  CouponActionButton.swift
+//  Wakup
+//
+//  Created by Guillermo Gutiérrez on 5/1/16.
+//  Copyright © 2016 Yellow Pineapple. All rights reserved.
+//
+
+import Foundation
+
+public class CouponActionButton: CenteredCodeIconButton {
+    
+}
