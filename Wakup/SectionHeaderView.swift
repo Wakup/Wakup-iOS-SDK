9b80df89c9f4cef1ec3e2fc8af523d669fcc5c4c
Guillermo Gutiérrez <guillermo.gutierrez@twincoders.com>
2019-09-03 17:36:51 +0200

    SectionHeaderView can now be customized using UIAppearance proxies and internationalized using Strings files

--- Wakup/SectionHeaderView.swift
+++ Wakup/SectionHeaderView.swift
@@ -12,8 +12,14 @@ open class SectionHeaderView: UICollectionReusableView {
 
     @IBOutlet weak open var titleLabel: UILabel!
     
-    open var title: String? {
-        didSet { titleLabel.text = self.title }
-    }
+    @objc open dynamic var title: String? { get { return titleLabel?.text } set { titleLabel?.text = newValue } }
+    @objc open dynamic var titleFont: UIFont? { get { return titleLabel?.font } set { titleLabel?.font = newValue } }
+    @objc open dynamic var titleColor: UIColor? { get { return titleLabel?.textColor } set { titleLabel?.textColor = newValue } }
     
+    open override func awakeFromNib() {
+        super.awakeFromNib()
+        if title?.count ?? 0 == 0 {
+            title = "RelatedOffers".i18n()
+        }
+    }
 }
